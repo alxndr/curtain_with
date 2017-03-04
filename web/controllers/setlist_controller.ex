7e3cbe1f036ff2d45640cc22070006b6d7c1b97d
Alexander <alxndr+github@gmail.com>
2017-03-03 21:05:12 -0800

    detect segues between songs; preserve song notes

--- web/controllers/setlist_controller.ex
+++ web/controllers/setlist_controller.ex
@@ -27,10 +27,11 @@ defmodule CurtainWith.SetlistController do
   def tailor_data([%{"setlistdata" => setlist_data} = show_data]) do
     setlist =
       setlist_data
-      |> escape_segues
-      |> strip_title_attributes
+      |> swap_segues
+      |> sanitize_title_attributes
       |> extract_elements
-      |> turn_into_arrays
+      |> Enum.reduce(%{}, &reduce_tuples_to_map_of_sets/2)
+      |> Map.delete(:current_set)
     Map.put(show_data, "setlist", setlist)
     |> Map.delete("setlistdata")
   end
@@ -43,12 +44,12 @@ defmodule CurtainWith.SetlistController do
     IO.inspect unrecognized_data
   end
 
-  def escape_segues(wacky_html) do
-    String.replace(wacky_html, " > ", " &gt; ")
+  def swap_segues(wacky_html) do
+    Regex.replace(~r{(</a>)( -?>) }, wacky_html, "\\2\\1 ")
   end
 
-  def strip_title_attributes(wacky_html) do
-    Regex.replace(~r{ title=(['"]).+?\1(>|\s+\w+=)}, wacky_html, "\\2")
+  def sanitize_title_attributes(wacky_html) do
+    Regex.replace(~r{ title=(['"])(.+?)\1(>|\s+\w+=)}, wacky_html, " title=\"\\2\"\\3")
   end
 
   # extracts just the elements for set markers and songs
@@ -57,20 +58,46 @@ defmodule CurtainWith.SetlistController do
     Floki.find(less_wacky_html, ".set-label, a")
   end
 
-  def turn_into_arrays(floki_tuples) do
-    Enum.reduce(floki_tuples, %{}, &reduce_tuples_to_map/2)
-    |> Map.delete(:current_set)
-  end
-
-  def reduce_tuples_to_map({tag, _, [set_label]}, acc) when tag === "span" do
+  # sets a bit of state. we are processing songs in played order, so
+  # the :current_set value tracks what set label we have most recently encountered.
+  # songs will be identified with this set until another set label is encountered.
+  def reduce_tuples_to_map_of_sets({tag, _, [set_label]}, acc) when tag === "span" do
     Map.merge acc, %{:current_set => set_label, set_label => []}
   end
-  def reduce_tuples_to_map({tag, _, [song_title]}, acc) when tag === "a" do
-    Map.put(acc, acc.current_set, append(Map.get(acc, acc.current_set), song_title))
+  def reduce_tuples_to_map_of_sets({tag, html_attributes_array, [song_title]}, acc) when tag === "a" do
+    html_attributes_map = Enum.into(html_attributes_array, %{})
+    song = build_song_map(song_title, html_attributes_map)
+    transformed_set =
+      acc
+      |> Map.get(acc.current_set)
+      |> append(song)
+    Map.put acc, acc.current_set, transformed_set
   end
-  def reduce_tuples_to_map(floki_tuple) do
+  def reduce_tuples_to_map_of_sets(floki_tuple) do
     IO.puts "!!!!!!! Unrecognized Floki tuple!"
     IO.inspect floki_tuple
+  end
+
+  def build_song_map(song_title, %{"title" => title_attribute} = html_attributes) do
+    just_the_title = Regex.replace(~r{ -?>$}, song_title, "")
+    title_attribute = String.trim(title_attribute)
+    a_map = if title_attribute === just_the_title do
+      %{title: just_the_title}
+    else
+      %{
+        title: just_the_title,
+        notes: title_attribute,
+      }
+    end
+    cond do
+      String.ends_with?(song_title, " ->") ->
+        %{segue: "->"}
+      String.ends_with?(song_title,  " >") ->
+        %{segue: ">"}
+      true ->
+        %{}
+    end
+    |> Map.merge a_map
   end
 
   def append(list, element) do
